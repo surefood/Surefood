18586e8adef4e6d652f7c2d983b8e1f9ec5d0b2e
Obasi Uche <obasiuche62@gmail.com>
2019-05-03 16:17:20 +0100

    Updates

---
+++ precache-manifest.eee7e078439c7c7ef4e625bc86bad3eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed679eef850efe454369fa874702efe5",
+    "url": "/Surefood/index.html"
+  },
+  {
+    "revision": "c61b916e232d12c14aff",
+    "url": "/Surefood/static/css/main.63bf5c68.chunk.css"
+  },
+  {
+    "revision": "2409ede63af559631465",
+    "url": "/Surefood/static/js/2.1802e650.chunk.js"
+  },
+  {
+    "revision": "c61b916e232d12c14aff",
+    "url": "/Surefood/static/js/main.68838653.chunk.js"
+  },
+  {
+    "revision": "4ad9f74d1cbe2562d156",
+    "url": "/Surefood/static/js/runtime~main.5959face.js"
+  },
+  {
+    "revision": "1a410cc39c6f0b31abc20f1463d8a720",
+    "url": "/Surefood/static/media/group.1a410cc3.png"
+  },
+  {
+    "revision": "f211cfe966f9f5bf5e1af350aebaefc4",
+    "url": "/Surefood/static/media/logo.f211cfe9.svg"
+  },
+  {
+    "revision": "34907e1f7ff5d68ccf75bc16f193b6eb",
+    "url": "/Surefood/static/media/surefood_desktop.34907e1f.png"
+  },
+  {
+    "revision": "fd92d135aa683b98ecbb32d4cfc7080b",
+    "url": "/Surefood/static/media/surefood_mobile.fd92d135.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/surefood.github.io/Surefood/precache-manifest.5adbe3df56f0395f47ceeeeb442820a6.js"
+  "/Surefood/precache-manifest.eee7e078439c7c7ef4e625bc86bad3eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/surefood.github.io/Surefood/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Surefood/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
